83a6c7f337dc4029fbbb15ba3fb5b0803a57db84
Sean Broestl <broestls@gmail.com>
2026-05-21 17:42:24 -0400

    Add a script for testing input/output

---
+++ scripts/boilerplate.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env -S uv run --script
+#
+# /// script
+# requires-python = ">=3.9"
+# dependencies = ["google-cloud-resource-manager>=1.14.2"]
+# ///
+
+import argparse
+import json
+import sys
+import logging
+import os
+
+def main():
+    # Initialize argument parser and dictionary-ize arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-payload', help='Payload from queue', required=True)
+    parser.add_argument('-apiKey', help='The apiKey of the integration', required=True)
+    parser.add_argument('-jsmUrl', help='The url', required=True)
+    parser.add_argument('-logLevel', help='Level of log', required=True)
+    parser.add_argument('--jecNamedPipe', help='Path of a pipe object you can use to send data back to the caller', required=False)
+    args, unknown = parser.parse_known_args()
+    args = vars(args)
+
+    if 'jecNamedPipe' in args:
+        payload = json.loads(args['payload'])
+        try:
+            with open(args['jecNamedPipe'], 'w', encoding="utf-8") as pipe:
+                pipe.write(json.dumps(payload))
+                logging.info(f"Message written to named pipe at {args['jecNamedPipe']}")
+        except Exception as e:
+            logging.error(f"Error writing to named pipe {args['jecNamedPipe']} with exception: {e}")
+            sys.exit(1)

abe68ec4cc56a68d1616c2a27d2a5080af79f1c6
Sean Broestl <broestls@gmail.com>
2026-07-02 09:35:50 -0700

    Update wrapper script to use script name as parameter

--- scripts/uv_wrapper.sh
+++ scripts/uv_wrapper.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-uv run "$@"
+# uv-wrapper.sh
+
+# 1. Grab the first argument, which will be the target Python script path
+TARGET_SCRIPT="$1"
+
+# 2. Shift the argument positional parameters left by 1.
+# This drops the Python script name from "$@" so it isn't passed twice.
+shift
+
+# 3. Execute uv run, feeding it the target script and the remaining JEC args
+uv run -- "$TARGET_SCRIPT" "$@"
